024395c1371c3a7d2207d50aea6d88a95943fe0e
Imoni McCorvey <imonimccorvey@Imonis-MacBook-Pro.local>
2017-11-27 20:53:01 -0500

    Fixes #6 There are bounds to detect when the player falls

---
+++ game/scenes/.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+
+	
+	set_process(true)
+
+
+func _on_outerbounds_body_enter( body ):
+	if body extends KinematicBody2D:
+		body.set_pos(Vector2(0,0))
+
+
